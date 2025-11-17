6339be8a22f5d97d7016f5fd52d2f290d6928298
MikeSymoDev <82706457+MikeSymoDev@users.noreply.github.com>
2025-11-17 08:38:29 +0100

    analizing of special documents

--- scripts/gemini_transcript_pdf.py
+++ scripts/gemini_transcript_pdf.py
@@ -20,7 +20,9 @@ total_out_tokens = 0
 input_cost_per_mio_in_dollars = 2.5
 output_cost_per_mio_in_dollars = 10
 
-input_dir = "../pdf_data_transcript/handschrift"
+# Change input directory to the specific folder
+
+input_dir = "../pdf_data_transcript/..."
 output_dir = "../answers/google_transcript"
 os.makedirs(output_dir, exist_ok=True)
 
